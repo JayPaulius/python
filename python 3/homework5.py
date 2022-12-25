dec02dee67b2e1edaab0e93ebf37236a7fc58685
Paul <jaypaulius@gmail.com>
2022-12-25 14:43:36 +0300

    commit

--- python 3/homework5.py
+++ python 3/homework5.py
@@ -3,20 +3,29 @@
 # - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 
 num = int(input("введите число: "))
-list1 = []
-list2 = []
-check = False
-for i in range(0, num + 1):
-    if i == 0:
-        list1.append(0)
-    elif i == 1:
-        list1.append(1)
-    else:
-        list1.append((list1[i - 2] - list1[i - 1]))
-for i in range(0, num):
-    if i in [0, 1]:
-        list2.append(1)
-    else:
-        list2.append(list2[i - 1] + list2[i - 2])
+list1 = [0, 1]
+for i in range(num - 1):
+    list1.append(list1[i] - list1[i + 1])
 list1.reverse()
-print(list1 + list2)
+for i in range(num - 1, num * 2 - 1):
+    list1.append(list1[i] + list1[i + 1])
+print(list1)
+
+# num = int(input("введите число: "))
+# list1 = []
+# list2 = []
+# check = False
+# for i in range(0, num + 1):
+#     if i == 0:
+#         list1.append(0)
+#     elif i == 1:
+#         list1.append(1)
+#     else:
+#         list1.append((list1[i - 2] - list1[i - 1]))
+# for i in range(0, num):
+#     if i in [0, 1]:
+#         list2.append(1)
+#     else:
+#         list2.append(list2[i - 1] + list2[i - 2])
+# list1.reverse()
+# print(list1 + list2)
